008dd438c0bff9740208b3602c63dba4db1d2c92
jaiz25 <jaizelle@gmail.com>
2016-02-26 05:58:13 +0000

    Created task 1

--- task_01.py
+++ task_01.py
@@ -7,9 +7,24 @@ import datetime
 
 CURDATE = None
 
+
 def get_current_date():
+    """Getting the current date.
+
+    Args:
+        no parameters(mixed): Doesn't take any parameters
+
+    Returns:
+        date: The current date
+
+    Example:
+
+    >>>import task_01
+    >>>task_01.get_current_date()
+    datetime.date(2015, 1, 1)
+    """
     return datetime.date.today()
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     CURDATE = get_current_date()
     print CURDATE
