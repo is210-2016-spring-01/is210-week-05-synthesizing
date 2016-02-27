9399387a1af64082834c27619c73823901ec40a8
ct3080a <coleen.thomas@spsmail.cuny.edu>
2016-02-27 04:08:20 +0000

    first pass

---
+++ task_01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A Date and Time Module"""
+
+import datetime
+
+CURDATE = None
+
+def get_current_date():
+    """function to return today's date on command line.
+
+    Args:
+
+    Returns:
+
+    Example:
+
+    """
+
+return datetime.date.today()date
