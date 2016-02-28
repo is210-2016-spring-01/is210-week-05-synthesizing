936f492c7f3f1908a45c3413ed0849be5a420570
paisely65 <spscuny@gmail.com>
2016-02-28 13:37:56 +0000

    This task is finished

--- task_01.py
+++ task_01.py
@@ -4,8 +4,31 @@
 
 import datetime
 CURDATE = None
+datetime.date.today()
+
+
 def get_current_date():
-    datetime.date.today()
-    CURDATE = get_current_date()
     return CURDATE
-    
+    """This functions calculates the date.
+
+    Args:
+        None
+
+    Return:
+        str: Arguments concatenated with commas.
+
+    Examples:
+    >>> import task_01
+    >>> print task_01.CURDATE
+    None
+
+    >>> task_01.get_current_date()
+    datetime.date(2015, 1, 1)
+    $ python -i task_01.py
+    2015-01-01
+    >>> CURDATE
+    datetime.date(2015, 1, 1)
+    """
+
+
+
