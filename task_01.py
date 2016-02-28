592120a01275f65715243997f2040d653d3ec2ae
paisely65 <spscuny@gmail.com>
2016-02-28 07:44:52 +0000

    this task is completed

---
+++ task_01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/ python
+# -*- coding: utf-8 -*-
+"""A really good docstring for showing the date"""
+
+import datetime
+CURDATE = None
+def get_current_date():
+    datetime.date.today()
+    CURDATE = get_current_date()
+    return CURDATE
+    
