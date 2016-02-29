a3d7d43e57af87b26f58bf84d53d2d9f507fbb93
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-02-29 09:39:37 +0000

    done?

---
+++ task_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""task 01"""
+
+
+import datetime
+
+CURDATE = None
+
+
+def get_current_date():
+    """function uses datetime.date.today()"""
+    return datetime.date.today()
