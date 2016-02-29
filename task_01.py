99dcad2420ca724b376f4eb2c2f28048664c0674
Sylvia Guzikowski <xiran-aneki@live.com>
2016-02-29 08:37:46 +0000

    corrected

--- task_01.py
+++ task_01.py
@@ -7,6 +7,22 @@ import datetime
 CURDATE = None
 
 def get_current_date():
+    """this function returns the date.
+
+    Args:
+        Takes no parameters!
+
+    Returns:
+        date: today's date
+
+    Examples:
+    
+        >>> import task_01
+        >>> print task_01.CURDATE
+        None
+
+        >>> task_01.get_current_date()
+        datetime.date(2015, 1, 1)
+    """  
     date = datetime.date.today()
     return date
-    
