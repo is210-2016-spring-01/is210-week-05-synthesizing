63f8f6e96be06e2e4aa5f45873b5ae811bf5f414
Sylvia Guzikowski <xiran-aneki@live.com>
2016-02-29 07:05:06 +0000

    done.. i think

---
+++ task_01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Doing things with datetime"""
+
+import datetime
+
+CURDATE = None
+
+def get_current_date():
+    date = datetime.date.today()
+    return date
+    
